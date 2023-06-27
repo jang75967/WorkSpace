e8f17b5b1574018c80f3a92a4a98d1c856fde8c5
JDG <jang75967@gmail.com>
2023-06-27 22:39:33 +0900

    클린아키텍쳐 분석 | grpc Client 통신 테스트 진행, 생명주기 관련 보충 필요

---
+++ CleanArchitecture/CleanArchitecture.Client/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press any Key to contiune...");
+            Console.ReadLine();
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:7277");
+            var client = new UsersGrpc.UsersGrpcClient(channel);
+            var reply = await client.GetUsersAsync(new GetUserRequest { });
+
+            await Console.Out.WriteLineAsync();
+
+            foreach (var user in reply.Users)
+            {
+                Console.WriteLine($"{user.Id}, {user.Name}, {user.Email}");
+            }
+
+            await Console.Out.WriteLineAsync();
+            Console.WriteLine("Press any Key to contiune...");
+            Console.ReadLine();
+        }
+    }
+}
