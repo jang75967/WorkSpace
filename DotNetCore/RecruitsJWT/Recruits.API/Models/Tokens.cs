45dbf7b441320018839ae08724a57bec35da6ed2
JDG <jang75967@gmail.com>
2023-07-03 22:41:29 +0900

    CleanArchitecture | ASP.NET JWT (JSON Web Token)

---
+++ DotNetCore/RecruitsJWT/Recruits.API/Models/Tokens.cs
@@ -0,0 +1,8 @@
+﻿namespace Recruits.API.Models
+{
+    public class Tokens
+    {
+        public string Token { get; set; } = default!;
+        public string RefreshToken { get; set; } = default!;
+    }
+}
---
+++ DotNetCore/RecruitsJWT/Recruits.API/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace Recruits.API.Models
+{
+    public class Users
+    {
+        public string Name { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
