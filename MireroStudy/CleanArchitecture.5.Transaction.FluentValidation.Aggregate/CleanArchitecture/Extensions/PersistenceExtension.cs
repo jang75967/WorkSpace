0ec3ece34331a6fe6c61ace6b87996ad03fdcb5c
장동계 <donggye.jang@mirero.co.kr>
2023-08-25 10:11:54 +0900

    5차 코드 수정

--- MireroStudy/CleanArchitecture.5.Transaction.FluentValidation.Aggregate/CleanArchitecture/Extensions/PersistenceExtension.cs
+++ MireroStudy/CleanArchitecture.5.Transaction.FluentValidation.Aggregate/CleanArchitecture/Extensions/PersistenceExtension.cs
@@ -7,11 +7,7 @@ public static class PersistenceExtension
 {
     public static IServiceCollection AddEFCore(this IServiceCollection services, IConfiguration configuration)
     {
-       // services.AddTransient<IApplicationDbContext, Infrastructure.EFCore.ApplicationDbContext>();
         services.AddPostgreSql(configuration, Assembly.GetExecutingAssembly().GetName().Name!);
-        //services.AddOracle(configuration);
-        //services.AddMsSql(configuration);
-        //services.AddMongoDB(configuration);
         return services;
     }
 }
