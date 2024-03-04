9b8806c699713eec0e52eee473d74b4bba5d57f5
장동계 <donggye.jang@mirero.co.kr>
2024-03-05 08:09:07 +0900

    UserControl, DependencyProperty 사용 예제

---
+++ MireroStudy/ConfigurateWatiTest/ConfigurateWatiTest/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ConfigurateWatiTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            //txtConfigAwait.Text = "Before configure Await";
+
+            //await DoSomethingAsync().ConfigureAwait(false);
+
+            //txtConfigAwait.Text = "After configure await set to false";
+
+            DoSomethingAsync().Wait();
+        }
+
+        private async Task DoSomethingAsync()
+        {
+            await Task.Delay(1000).ConfigureAwait(false);   
+
+            Debug.WriteLine("Continuation task");
+        }
+    }
+}
---
+++ MireroStudy/CustomControl/CustomContrl/LabelTextBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomContrl;
+
+/// <summary>
+/// LabelTextBox.xaml에 대한 상호 작용 논리
+/// </summary>
+public partial class LabelTextBox : UserControl
+{
+    public static readonly DependencyProperty LabelProperty =
+                DependencyProperty.Register("Label", typeof(string), typeof(LabelTextBox));
+
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(LabelTextBox));
+
+    public static readonly DependencyProperty OrientationProperty =
+        DependencyProperty.Register("Orientation", typeof(Orientation), typeof(LabelTextBox),
+            new FrameworkPropertyMetadata(Orientation.Vertical));
+
+    public string Label
+    {
+        get { return (string)GetValue(LabelProperty); }
+        set { SetValue(LabelProperty, value); }
+    }
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public Orientation Orientation
+    {
+        get { return (Orientation)GetValue(OrientationProperty); }
+        set { SetValue(OrientationProperty, value); }
+    }
+
+    public LabelTextBox()
+    {
+        InitializeComponent();
+    }
+}
