2a26e46808226b69f652a0473f69040aca6a8c66
장동계 <donggye.jang@mirero.co.kr>
2024-03-13 15:04:11 +0900

    CancellationTokenExample 추가

---
+++ MireroStudy/CancellationTokenExample/CancellationTokenExample/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Numerics;
+
+namespace CancellationTokenExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new();
+
+            LongWork(long.MaxValue - 1, cancellationTokenSource.Token);
+
+            Console.WriteLine("Press any key to cancel...");
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+            Console.WriteLine("End-of-work");
+        }
+
+        static async Task LongWork(long to, CancellationToken token)
+        {
+            Console.WriteLine("Starting...");
+            await Task.Factory.StartNew(() => GetSum(to, token));
+            Console.WriteLine("Completed...");
+        }
+
+        private static BigInteger GetSum(long last, CancellationToken token)
+        {
+            BigInteger sum = 0;
+
+            for (long i = 0; i < last; i++)
+            {
+                if (token.IsCancellationRequested == true)
+                {
+                    break;
+                }
+
+                i++;
+                sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ MireroStudy/CancellationTokenExample/CancellationTokenTimeExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace CancellationTokenTimeExample;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        CancellationTokenSource cancellationTokenSource = new();
+
+        // 5초 후에 작업이 취소되도록 설정
+        cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+
+        try
+        {
+            await LongWork(long.MaxValue - 1, cancellationTokenSource.Token);
+            Console.WriteLine("Completed...");
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Task was canceled due to timeout.");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine();
+    }
+
+    static async Task LongWork(long to, CancellationToken token)
+    {
+        Console.WriteLine("Starting...");
+        await Task.Run(() => GetSum(to, token), token);
+    }
+
+    private static BigInteger GetSum(long last, CancellationToken token)
+    {
+        BigInteger sum = 0;
+
+        for (long i = 0; i < last; i++)
+        {
+            // 작업 취소 확인
+            token.ThrowIfCancellationRequested();
+
+            i++;
+            sum += i;
+        }
+
+        return sum;
+    }
+}
