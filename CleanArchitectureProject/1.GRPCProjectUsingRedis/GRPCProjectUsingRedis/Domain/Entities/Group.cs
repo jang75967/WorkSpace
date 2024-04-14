6b1b73e959cbd6ee268110fff89b259ffbd92d59
JDG <jang75967@gmail.com>
2024-04-15 08:25:49 +0900

    BaseEntity, Group Entity 추가

---
+++ CleanArchitectureProject/1.GRPCProjectUsingRedis/GRPCProjectUsingRedis/Domain/Entities/Group.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities
+{
+    public class Group : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+    }
+}
--- CleanArchitectureProject/1.GRPCProjectUsingRedis/GRPCProjectUsingRedis/Domain/Entities/User.cs
+++ CleanArchitectureProject/1.GRPCProjectUsingRedis/GRPCProjectUsingRedis/Domain/Entities/User.cs
@@ -1,8 +1,7 @@
 ﻿namespace Domain.Entities
 {
-    public class User
+    public class User : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string Email { get; set; } = default!;
         public string Password { get; set; } = default!;
