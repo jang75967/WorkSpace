798fa781e090d3d28ebc49354e4c22f401b8f564
JDG <jang75967@gmail.com>
2024-04-18 09:06:17 +0900

    마이그레이션 완료된 데이터 추가

---
+++ CleanArchitectureProject/1.GRPCProjectUsingRedis/GRPCProjectUsingRedis/InfraStructure/Data/Migrations/20240417235040_InitializeDatabase.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace InfraStructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "JDGPostgresEFCore");
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                schema: "JDGPostgresEFCore",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "JDGPostgresEFCore",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Password = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "JDGPostgresEFCore",
+                table: "Groups",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "축구" },
+                    { 2, "농구" },
+                    { 3, "야구" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "JDGPostgresEFCore",
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Password" },
+                values: new object[,]
+                {
+                    { 1, "jdg1@gmail.com", "jdg1", null },
+                    { 2, "jdg2@gmail.com", "jdg2", null },
+                    { 3, "jdg3@gmail.com", "jdg3", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Groups",
+                schema: "JDGPostgresEFCore");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "JDGPostgresEFCore");
+        }
+    }
+}
